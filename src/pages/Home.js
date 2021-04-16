29dcacecad7a49469707c0ca0aadc81a2e3535e3
Rangga Eki Syahrial <syahrialrangga11@gmail.com>
2021-04-16 18:22:20 +0700

    Create folder structure to accomodate technology (redux, react-router-dom, custom bootstrap)

---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+    </div>
+  );
+}
