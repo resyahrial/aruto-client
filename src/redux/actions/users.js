bc276eaff3ea841b60753deb7da76736358965c4
robbybenta <robbybenta7@gmail.com>
2021-04-18 22:31:45 +0700

    done consume api login

--- src/redux/actions/users.js
+++ src/redux/actions/users.js
@@ -1,5 +1,19 @@
 import axios from "../axios";
 
-// export const fetchArts = (payload) => (dispatch) => {
-// code
-// };
+export const login = (payload) => (dispatch) => {
+    const data = {
+        email:payload.email,
+        password:payload.password
+      }
+      dispatch({ type: "users/isLoading" });
+      axios
+      .post(`${process.env.REACT_APP_URL}login`,data)
+      .then(res=>{
+          console.log(res,'res');
+          localStorage.setItem('access_token', res.data.access_token)
+      })
+      .catch(err=>  dispatch({ type: "users/error" }, err))
+      .finally(() => {
+        dispatch({ type: "users/isLoading" });
+      });
+};
--- src/redux/reducers/usersReducer.js
+++ src/redux/reducers/usersReducer.js
@@ -10,7 +10,10 @@ const reducer = (state = initialState, { type, payload }) => {
   switch (type) {
     case "users/register":
       return { ...state, ...payload };
-
+      case "users/isLoading":
+      return { ...state, isLoading: !state.isLoading };
+      case "users/error":
+      return { ...state, error: payload };
     default:
       return state;
   }
