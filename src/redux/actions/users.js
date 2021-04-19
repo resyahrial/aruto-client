dd7d5413153950ea93d448d55a1bea10250138aa
robbybenta <robbybenta7@gmail.com>
2021-04-19 10:40:12 +0700

    done consume api register

--- src/redux/actions/users.js
+++ src/redux/actions/users.js
@@ -12,8 +12,28 @@ export const login = (payload) => (dispatch) => {
           console.log(res,'res');
           localStorage.setItem('access_token', res.data.access_token)
       })
-      .catch(err=>  dispatch({ type: "users/error" }, err))
+      .catch(err=>  dispatch({ type: "users/error" ,payload:err}))
       .finally(() => {
         dispatch({ type: "users/isLoading" });
       });
+};
+export const register = (payload) => (dispatch) => {
+  const data = {
+    full_name:payload.fullname,
+    username:payload.username,
+      email:payload.email,
+      password:payload.password,
+   
+    }
+    console.log(data,'register');
+    dispatch({ type: "users/isLoading" });
+    axios
+    .post(`${process.env.REACT_APP_URL}register`,data)
+    .then(res=>{
+    })
+    .catch(err=>  dispatch({ type: "users/error" }, err))
+    .finally(() => {
+      // dispatch(login({email:payload.email,password:payload.password}))
+      dispatch({ type: "users/isLoading" });
+    });
 };
