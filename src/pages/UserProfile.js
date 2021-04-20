3012a3e5e6e5b661f345a48310f04f1fe7475b8f
naufalwibs <naufalwibisono95@gmail.com>
2021-04-20 20:01:20 +0700

    Done Minus styling

--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -1,18 +1,49 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import { fetchUserById } from "../redux/actions/users";
-import { MyArtCard, AddArt } from "../components";
+import { fetchTransactionHistory } from "../redux/actions/transactions";
+import { MyArtCard, AddArt, MyTransactionCard } from "../components";
 import { useHistory } from "react-router-dom";
 
 export default function UserProfile() {
   const dispatch = useDispatch();
-  const {userDataById, isLoading} = useSelector((state) => state.users);
+  const { userDataById, isLoading } = useSelector((state) => state.users);
+  const transactionHistory = useSelector(
+    (state) => state.transactions.myHistory
+  );
+  const [myWorks, setMyWorks] = useState(true);
+  const [myPurchases, setMyPurchase] = useState(false);
+  const [myFavorites, setMyFavorites] = useState(false);
   const history = useHistory();
 
+  console.log(transactionHistory);
+
   useEffect(() => {
+    dispatch(fetchTransactionHistory("Test"));
     dispatch(fetchUserById(localStorage.access_token));
   }, [dispatch]);
+
+  const viewMyWorks = (e) => {
+    e.preventDefault();
+    setMyWorks(true);
+    setMyPurchase(false);
+    setMyFavorites(false);
+  };
+
+  const viewMyPurchases = (e) => {
+    e.preventDefault();
+    setMyWorks(false);
+    setMyPurchase(true);
+    setMyFavorites(false);
+  };
+
+  const viewMyFavorites = (e) => {
+    e.preventDefault();
+    setMyWorks(false);
+    setMyPurchase(false);
+    setMyFavorites(true);
+  };
 
   if (!localStorage.access_token) {
     history.push("/login");
@@ -21,21 +52,18 @@ export default function UserProfile() {
   return (
     <>
       <section id="user-profile">
-        {
-          isLoading &&       
-          <div class="text-center">
-            <div class="spinner-border" role="status">
-              <span class="sr-only">Loading...</span>
+        {isLoading && (
+          <div className="text-center">
+            <div className="spinner-border" role="status">
+              <span className="sr-only">Loading...</span>
             </div>
           </div>
-        }
+        )}
         <div className="container pt-3">
           <div className="row">
             <div className="col-lg-3 pr-3">
               <div className="border rounded-lg">
-                {
-                  JSON.stringify(isLoading)
-                }
+                {/* {JSON.stringify(isLoading)} */}
                 {/* profile */}
                 <div className="d-flex align-items-center py-3 px-3">
                   <img
@@ -63,18 +91,30 @@ export default function UserProfile() {
                 <nav className="navbar navbar-expand-lg navbar-light">
                   <ul className="navbar-nav mr-auto">
                     <li className="nav-item active">
-                      <a className="nav-link" href="true">
+                      <a
+                        onClick={(e) => viewMyWorks(e)}
+                        className="nav-link"
+                        href="true"
+                      >
                         My Works <span className="sr-only">(current)</span>
                       </a>
                     </li>
                     <li className="nav-item">
-                      <a className="nav-link" href="true">
+                      <a
+                        onClick={(e) => viewMyPurchases(e)}
+                        className="nav-link"
+                        href="true"
+                      >
                         My Purchase
                       </a>
                     </li>
                     <li className="nav-item">
-                      <a className="nav-link" href="true">
-                        My Favorite
+                      <a
+                        onClick={(e) => viewMyFavorites(e)}
+                        className="nav-link"
+                        href="true"
+                      >
+                        My Favorites
                       </a>
                     </li>
                   </ul>
@@ -82,16 +122,91 @@ export default function UserProfile() {
               </div>
               <div className="col-lg">
                 <div className="row">
-                  {userDataById?.data?.arts.length === 0 ? (
-                    <>
-                      <div className="col-lg text-center pt-5">
-                        <h3>There's No Art to Show</h3>
-                      </div>
-                    </>
+                  {myWorks ? (
+                    userDataById?.data?.arts.length === 0 ? (
+                      <>
+                        <div className="col-lg text-center pt-5">
+                          <h3>There's No Art to Show</h3>
+                        </div>
+                      </>
+                    ) : (
+                      userDataById?.data?.arts?.map((art) => {
+                        return <MyArtCard key={art._id} art={art} />;
+                      })
+                    )
                   ) : (
-                    userDataById?.data?.arts?.map((art) => {
-                      return <MyArtCard key={art._id} art={art} />;
-                    })
+                    ""
+                  )}
+
+                  {myPurchases ? (
+                    transactionHistory.length === 0 ? (
+                      <>
+                        <div className="col-lg text-center pt-5">
+                          <h3>There's No Purchase History to Show</h3>
+                        </div>
+                      </>
+                    ) : (
+                      <div className="col-lg-12">
+                        <table className="table table-striped">
+                          <thead>
+                            <tr>
+                              <th scope="col">#Transaction-ID</th>
+                              <th scope="col">Item</th>
+                              <th scope="col">Shipment Address</th>
+                              <th scope="col">Transaction Amount</th>
+                              <th scope="col">Payment Status</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            {transactionHistory?.map((history) => {
+                              return (
+                                <MyTransactionCard
+                                  key={history._id}
+                                  history={history}
+                                />
+                              );
+                            })}
+                          </tbody>
+                        </table>
+                      </div>
+                    )
+                  ) : (
+                    ""
+                  )}
+                  {myFavorites ? (
+                    userDataById?.data?.arts.length === 0 ? (
+                      <>
+                        <div className="col-lg text-center pt-5">
+                          <h3>There's No Favorites to Show</h3>
+                        </div>
+                      </>
+                    ) : (
+                      <div className="col-lg-12">
+                        <table className="table table-striped">
+                          <thead>
+                            <tr>
+                              <th scope="col">#Transaction-ID</th>
+                              <th scope="col">Item</th>
+                              <th scope="col">Shipment Address</th>
+                              <th scope="col">Transaction Amount</th>
+                              <th scope="col">Payment Status</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            {transactionHistory?.map((history) => {
+                              return (
+                                <MyTransactionCard
+                                  key={history._id}
+                                  history={history}
+                                />
+                              );
+                            })}
+                          </tbody>
+                        </table>
+                      </div>
+                    )
+                  ) : (
+                    ""
                   )}
                 </div>
               </div>
