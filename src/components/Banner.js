b46551fd034f18e926bd71ff68849d43810f7a2e
robbybenta <robbybenta7@gmail.com>
2021-04-20 15:59:14 +0700

    revisi minnor

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -20,14 +20,21 @@ export default function Banner() {
         <div>
           <img
             src="https://cdn.shopify.com/s/files/1/0255/3331/7156/files/TieDyeForThis.jpg?v=1613629456"
-            className="d-block w-100"
+            className="d-block w-100 height-banner"
             alt="..."
           />
         </div>
         <div>
           <img
-            src="https://cdn.shopify.com/s/files/1/0255/3331/7156/files/TieDyeForThis.jpg?v=1613629456"
-            className="d-block w-100"
+            src="https://cdn.shopify.com/s/files/1/0255/3331/7156/files/LOOKBOOK-SU-2_3b2cf567-93b4-4cd9-8b0d-9b3760f5201d.jpg?v=1618906795"
+            className="d-block w-100 height-banner"
+            alt="..."
+          />
+        </div>
+        <div>
+          <img
+            src="https://i.pinimg.com/originals/9a/71/6f/9a716f90fc2e24079b8960168d5ea089.jpg"
+            className="d-block w-100 height-banner"
             alt="..."
           />
         </div>
