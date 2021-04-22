aa0e563f425f109310dd2c91916f07c2ec802741
naufalwibs <naufalwibisono95@gmail.com>
2021-04-22 19:41:23 +0700

    Revisi Tolong cek gan fungsionalitasnya

--- src/components/MyTransactionCard.js
+++ src/components/MyTransactionCard.js
@@ -1,29 +1,35 @@
 import React from "react";
 
-export default function MyTransactionCart({ history }) {
+export default function MyTransactionCard({ history }) {
   console.log(history);
 
   return (
     <>
       <tr>
-        <th scope="row">{history._id}</th>
+        <th scope="row">{history?.transaction?._id}</th>
         <td>
-          {history.arts.map((transaction) => {
+          <img
+            className="img-trans"
+            src={history?.artData?.image_url}
+            alt="img"
+          />
+          <p>{history?.artData?.title}</p>
+          {history?.transaction?.arts?.map((transaction, i) => {
             return (
-              <p>
+              <p key={i}>
                 {transaction.item} {transaction.color} x {transaction.quantity}
               </p>
             );
           })}
         </td>
-        <td>{history.address}</td>
+        <td>{history?.transaction?.address}</td>
         <td>
           {new Intl.NumberFormat("id-Rp", {
             style: "currency",
             currency: "IDR",
-          }).format(history.gross_amount)}
+          }).format(history?.transaction?.gross_amount)}
         </td>
-        <td>{history.status}</td>
+        <td>{history?.transaction?.status}</td>
       </tr>
     </>
   );
